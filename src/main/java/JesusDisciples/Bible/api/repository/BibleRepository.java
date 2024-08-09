e84b5906dadebc646eaa2503c63df1103cea1349
RiverCastle <rkdtjd12@gmail.com>
2024-08-09 16:06:55 +0900

    feat: DB 연결

---
+++ src/main/java/JesusDisciples/Bible/api/repository/BibleRepository.java
@@ -0,0 +1,9 @@
+package JesusDisciples.Bible.api.repository;
+
+import JesusDisciples.Bible.api.entity.Bible;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BibleRepository extends JpaRepository<Bible, Long> {
+}
