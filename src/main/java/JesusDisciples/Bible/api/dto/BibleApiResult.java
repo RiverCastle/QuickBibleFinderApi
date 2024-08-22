c1f0171970b2d710d78b555cdf36171144183ee6
RiverCastle <rkdtjd12@gmail.com>
2024-08-22 13:38:39 +0900

    refactor: entity로 dto생성 메서드 추가

--- src/main/java/JesusDisciples/Bible/api/dto/BibleApiResult.java
+++ src/main/java/JesusDisciples/Bible/api/dto/BibleApiResult.java
@@ -1,5 +1,6 @@
 package JesusDisciples.Bible.api.dto;
 
+import JesusDisciples.Bible.api.entity.Bible;
 import lombok.Data;
 
 @Data
@@ -10,14 +11,48 @@ public class BibleApiResult {
     private String korTitle;
     private String engTitle;
 
-    private String chapter;
-    private String verse;
+    private Integer chapter;
+    private Integer verse;
     private String content;
 
-    public BibleApiResult(String engAbbr, String chapter, String verse, String content) {
+    public static BibleApiResult fromEntity(Bible entity) {
+        BibleApiResult result = new BibleApiResult();
+        result.setKorTitle(entity.getKorTitle());
+        result.setEngTitle(entity.getEngTitle());
+        result.setKorAbbr(entity.getKorAbbr());
+        result.setEngAbbr(entity.getEngAbbr());
+        result.setChapter(entity.getChapter());
+        result.setVerse(entity.getVerse());
+        result.setContent(entity.getContent());
+
+        return result;
+    }
+
+    public void setKorAbbr(String korAbbr) {
+        this.korAbbr = korAbbr;
+    }
+
+    public void setEngAbbr(String engAbbr) {
         this.engAbbr = engAbbr;
+    }
+
+    public void setKorTitle(String korTitle) {
+        this.korTitle = korTitle;
+    }
+
+    public void setEngTitle(String engTitle) {
+        this.engTitle = engTitle;
+    }
+
+    public void setChapter(Integer chapter) {
         this.chapter = chapter;
+    }
+
+    public void setVerse(Integer verse) {
         this.verse = verse;
+    }
+
+    public void setContent(String content) {
         this.content = content;
     }
 }
