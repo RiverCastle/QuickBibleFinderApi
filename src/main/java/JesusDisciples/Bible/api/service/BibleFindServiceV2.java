b534f38e5e3a494a9cfc2b43ec77a55ed3cc58ca
RiverCastle <rkdtjd12@gmail.com>
2024-08-23 11:58:22 +0900

    refactor: 불필요 코드 제거

--- src/main/java/JesusDisciples/Bible/api/service/BibleFindServiceV2.java
+++ src/main/java/JesusDisciples/Bible/api/service/BibleFindServiceV2.java
@@ -13,15 +13,15 @@ public class BibleFindServiceV2 {
 
     private final BibleRepository bibleRepository;
     public BibleApiResult[] getThisVerses(BibleFindReqDto bibleFindReqDto) {
-        System.out.println("bibleFindReqDto = " + bibleFindReqDto.toString());
         String engAbbr = bibleFindReqDto.getEngAbbr();
         String[] verseInfo = bibleFindReqDto.getVerseInfo().split(":");
         int chapter = Integer.parseInt(verseInfo[0]);
         int targetVerse = Integer.parseInt(verseInfo[1]);
+
         Bible entity = bibleRepository.findByEngAbbrAndChapterAndVerse(engAbbr, chapter, targetVerse);
-        System.out.println("entity = " + entity.toString());
         BibleApiResult[] result = new BibleApiResult[1];
         result[0] = BibleApiResult.fromEntity(entity);
+
         return result;
     }
 }
